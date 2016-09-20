d6a90bbf0ed7377e27bef0b184eef108838f7233
Shogo Nagasaka <nagasaka.shogo@donuts.ne.jp>
2016-09-20 11:17:34 +0900

    Disable error logging

--- stream/Apple/StreamSession.mm
+++ stream/Apple/StreamSession.mm
@@ -205,15 +205,15 @@ namespace videocore {
             }
             if(event & NSStreamEventErrorOccurred) {
                 setStatus(kStreamStatusErrorEncountered, true);
-                if (NSIS(m_inputStream).streamError) {
-                    NSLog(@"Input stream error:%@", NSIS(m_inputStream).streamError);
-                }
-                if (NSOS(m_outputStream).streamError) {
-                    NSLog(@"Output stream error:%@", NSIS(m_outputStream).streamError);
-                }
+                // if (NSIS(m_inputStream).streamError) {
+                //     NSLog(@"Input stream error:%@", NSIS(m_inputStream).streamError);
+                // }
+                // if (NSOS(m_outputStream).streamError) {
+                //     NSLog(@"Output stream error:%@", NSIS(m_outputStream).streamError);
+                // }
             }
         }
-        
+
         void
         StreamSession::startNetwork()
         {
